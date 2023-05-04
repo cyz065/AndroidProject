1ea8a11d04879f3f4718088eb1cda109c57f6d1f
PJH <parkjh0858@gmail.com>
2023-05-05 00:16:11 +0900

    calendar

--- WinWin/app/src/main/java/com/management/winwin/Card/WorkInfoAdapter.kt
+++ WinWin/app/src/main/java/com/management/winwin/Card/WorkInfoAdapter.kt
@@ -2,27 +2,29 @@ package com.management.winwin.Card
 
 import android.content.Context
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.management.winwin.R
 import com.management.winwin.databinding.InfoItemBinding
 import java.text.DecimalFormat
 
-class WorkInfoAdapter(val context: Context, private val workList:ArrayList<Work>) : RecyclerView.Adapter<WorkInfoAdapter.ViewHolder>() {
+class WorkInfoAdapter(val context: Context, private val workList:ArrayList<Work>) : RecyclerView.Adapter<WorkInfoAdapter.ViewHolder>(){
     private val decimalFormat = DecimalFormat("#,###")
     private val colors = context.resources.getIntArray(R.array.cardColors)
     private var index = 0
 
-    inner class ViewHolder(private val binding: InfoItemBinding) : RecyclerView.ViewHolder(binding.root) {
+
+    inner class ViewHolder(val binding: InfoItemBinding) : RecyclerView.ViewHolder(binding.root) {
         fun bind(info:Work) {
             binding.workSite.text = info.workSite
             val moneyFormat = decimalFormat.format(info.money.toDouble())
             val sb = StringBuilder()
             sb.append("\u20A9").append(moneyFormat.toString())
             binding.money.text = sb.toString()
+            index %= colors.size
             binding.cardView.setCardBackgroundColor(colors[index])
         }
-
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
